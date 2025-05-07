f7eb8cf1d0da5be3c32cd5eaaf0fa7d35effba6d
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-05-07 14:27:56 +0300

    implemented cache invalidation if a user deposits or withdraws from an account. Deposit and withdraw are now smart enough to detect whether the requested amount matches the currency type of the account and if not, convert it to match the account currency type

--- src/main/kotlin/com/bank/transaction/TransactionsService.kt
+++ src/main/kotlin/com/bank/transaction/TransactionsService.kt
@@ -1,10 +1,13 @@
 package com.bank.transaction
 
 import com.bank.account.AccountRepository
+import com.bank.account.AccountResponse
 import com.bank.currency.CurrencyRepository
 import com.bank.exchange.ExchangeRateApi
 import com.bank.promocode.PromoCodeRepository
+import com.bank.serverMcCache
 import com.bank.user.UserRepository
+import com.hazelcast.logging.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Service
 import java.math.BigDecimal
 import java.math.RoundingMode
 import java.time.LocalDateTime
+private val  loggerAccount = Logger.getLogger("account")
 
 @Service
 class TransactionsService(
@@ -28,6 +32,11 @@ class TransactionsService(
         val account = accountRepository.findByAccountNumber(request.accountNumber)
             ?: return ResponseEntity.badRequest().body(mapOf("error" to "account with number ${request.accountNumber} was not found"))
 
+        val requestedCurrency = currencyRepository.findByCountryCode(request.countryCode)
+            ?: return ResponseEntity.badRequest().body(mapOf("error" to "Currency not supported"))
+
+        val promoCode = promoCodeRepository.findByCode(103)
+
         if (account.user.id != userId) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body(mapOf("error" to "unauthorized access"))
         }
@@ -36,18 +45,22 @@ class TransactionsService(
             return ResponseEntity.badRequest().body(mapOf("error" to "account is not active..."))
         }
 
-        if (account.currency.countryCode != request.countryCode) {
-            return ResponseEntity.badRequest().body(mapOf("error" to "request currency does not match your accounts currency"))
-        }
-
         if (request.amount < BigDecimal("1.000") || request.amount > BigDecimal("100000.000")) {
-            return ResponseEntity.badRequest().body(mapOf("error" to "Amount must be between ${account.currency.countryCode}1 and ${account.currency.countryCode}100,000"))
+            return ResponseEntity.badRequest().body(mapOf("error" to "amount must be between ${account.currency.countryCode}1 and ${account.currency.countryCode}100,000"))
+        }
+//        if (account.currency.countryCode != request.countryCode) {
+//            return ResponseEntity.badRequest().body(mapOf("error" to "request currency does not match your accounts currency"))
+//        }
+
+        val (finalAmount, wasConverted) = if (account.currency.countryCode != request.countryCode) {
+            val conversionRate = exchangeRateApi.getRate(request.countryCode, account.currency.countryCode)
+            request.amount.multiply(conversionRate).setScale(3, RoundingMode.HALF_UP) to true
+        } else {
+            request.amount to false
         }
 
-        account.balance += request.amount
+        account.balance += finalAmount
         accountRepository.save(account)
-
-        val promoCode = promoCodeRepository.findByCode(103)
 
         transactionRepository.save(TransactionEntity(
             sourceAccount = null,
@@ -58,15 +71,29 @@ class TransactionsService(
             promoCode = promoCode,
             status = TransactionStatus.COMPLETED,
             transactionType = TransactionType.DEPOSIT
-
         ))
 
-        return ResponseEntity.ok(mapOf("message" to "${account.currency.symbol}${request.amount} has been deposited successfully into your account, new balance: ${account.currency.symbol}${account.balance}"))
+        val message = if (wasConverted) {
+            "Converted ${requestedCurrency.symbol}${request.amount} to ${account.currency.symbol}${finalAmount} and deposited successfully. new balance: ${account.currency.symbol}${account.balance}"
+        } else {
+            "${account.currency.symbol}${finalAmount} deposited successfully. new balance: ${account.currency.symbol}${account.balance}"
+        }
+
+        val accountCache = serverMcCache.getMap<Long, List<AccountResponse>>("account")
+        loggerAccount.info("user=$userId deposited into account=${account.accountNumber}...invalidating cache")
+        accountCache.remove(userId)
+        return ResponseEntity.ok(mapOf("message" to message))
     }
+
 
     fun withdrawAccount(request: DepositRequest, userId: Long?): ResponseEntity<*> {
         val account = accountRepository.findByAccountNumber(request.accountNumber)
             ?: return ResponseEntity.badRequest().body(mapOf("error" to "account with number ${request.accountNumber} was not found"))
+
+        val requestedCurrency = currencyRepository.findByCountryCode(request.countryCode)
+            ?: return ResponseEntity.badRequest().body(mapOf("error" to "Currency not supported"))
+
+        val promoCode = promoCodeRepository.findByCode(104)
 
         if (account.user.id != userId) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body(mapOf("error" to "Unauthorized access"))
@@ -76,9 +103,9 @@ class TransactionsService(
             return ResponseEntity.badRequest().body(mapOf("error" to "account is not active..."))
         }
 
-        if (account.currency.countryCode != request.countryCode) {
-            return ResponseEntity.badRequest().body(mapOf("error" to "request currency does not match your accounts currency"))
-        }
+//        if (account.currency.countryCode != request.countryCode) {
+//            return ResponseEntity.badRequest().body(mapOf("error" to "request currency does not match your accounts currency"))
+//        }
 
         if (request.amount < BigDecimal("1.000") || request.amount > BigDecimal("100000.000")) {
             return ResponseEntity.badRequest().body(mapOf("error" to "amount must be between ${account.currency.countryCode}1 and ${account.currency.countryCode}100,000"))
@@ -88,10 +115,15 @@ class TransactionsService(
             return ResponseEntity.badRequest().body(mapOf("error" to "insufficient balance"))
         }
 
-        account.balance -= request.amount
-        accountRepository.save(account)
+        val (finalAmount, wasConverted) = if (account.currency.countryCode != request.countryCode) {
+            val conversionRate = exchangeRateApi.getRate(request.countryCode, account.currency.countryCode)
+            request.amount.multiply(conversionRate).setScale(3, RoundingMode.HALF_UP) to true
+        } else {
+            request.amount to false
+        }
 
-        val promoCode = promoCodeRepository.findByCode(104)
+        account.balance -= finalAmount
+        accountRepository.save(account)
 
         transactionRepository.save(TransactionEntity(
             sourceAccount = account,
@@ -105,6 +137,16 @@ class TransactionsService(
 
         ))
 
-        return ResponseEntity.ok(mapOf("message" to "${account.currency.symbol}${request.amount} has been withdrew successfully from your account, new balance: ${account.currency.symbol}${account.balance}"))
+        val message = if (wasConverted) {
+            "Converted ${requestedCurrency.symbol}${request.amount} to ${account.currency.symbol}${finalAmount} and withdrawn successfully. new balance: ${account.currency.symbol}${account.balance}"
+        } else {
+            "${account.currency.symbol}${finalAmount} withdrawn successfully. new balance: ${account.currency.symbol}${account.balance}"
+        }
+
+        val accountCache = serverMcCache.getMap<Long, List<AccountResponse>>("account")
+        loggerAccount.info("user=$userId withdrew from account=${account.accountNumber}...invalidating cache")
+        accountCache.remove(userId)
+
+        return ResponseEntity.ok(mapOf("message" to message))
     }
 }
